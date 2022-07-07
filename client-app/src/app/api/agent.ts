8ac948556be311c3b19c909c069fc348fe61eeb9
Jimi Riihiluoma <jimi.riihiluoma@hotmail.com>
2022-07-07 16:58:34 +0300

    Added client side user bio editing

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -98,7 +98,8 @@ const Profiles = {
         })
     },
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+    updateProfile: (profile: Partial<Profile>) => requests.put<void>(`/profiles`, profile)
 }
 
 const agent = {
